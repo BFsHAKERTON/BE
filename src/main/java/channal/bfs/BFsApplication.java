71c9f50fd814389ebd96d5750714d174e27eab3f
Crom <doyoung.yang2005@gmail.com>
2025-11-06 02:00:57 +0900

    Chore : 초기세팅 공유 초기 프로젝트 생성

---
+++ src/main/java/channal/bfs/BFsApplication.java
@@ -0,0 +1,13 @@
+package channal.bfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BFsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BFsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/channal/bfs/BFsApplicationTests.java
@@ -0,0 +1,13 @@
+package channal.bfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BFsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
